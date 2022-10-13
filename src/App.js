3f763500880817a59c4dcf01cadb56447532cabe
Eugene <lamew128@gmail.com>
2022-10-13 19:56:31 -0400

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import TopBar from './components/TopBar/TopBar';
 import Home from './components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <TopBar />
-      <Home />
 
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <TopBar />
+            <h1>123</h1>
+          </Route>
+          <Route path="/">
+            <TopBar />
+            <Home />
+          </Route>
+
+        </Switch>
+        
+      </div>
+    </Router>
+    
   );
 }
 
