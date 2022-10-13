bf56c39fceab3019af2784e876ed55cdcaef3f8f
Eugene <lamew128@gmail.com>
2022-10-13 19:38:18 -0400

    Home page prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import TopBar from './TopBar'
+import TopBar from './components/TopBar/TopBar';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
       <TopBar />
+      <Home />
+
     </div>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className="home_container">
+        
+        <img 
+            className='home_image'
+            src='https://static.vecteezy.com/system/resources/previews/001/849/553/original/modern-gold-background-free-vector.jpg' 
+            alt=""
+         />
+
+         <div className="home_row">
+            <Product 
+              title="Gundam Exia HG 1/144"
+              price={100.99}
+              image='https://ca.super-hobby.com/zdjecia/7/7/8/36187_rd.jpg'
+              rating={5}
+            />
+            <Product
+              title="Gundam Bael Metal Robot Tamashi 1/144"
+              price={999.99}
+              image="https://a.rimg.com.tw/s2/5/4e/64/22152614447716_386.jpg"
+              rating={5}
+            />
+         </div>
+
+         <div className="home_row">
+            <Product
+              title="Gundam Bael Metal Robot Tamashi 1/144"
+              price={999.99}
+              image="https://a.rimg.com.tw/s2/5/4e/64/22152614447716_386.jpg"
+              rating={1}
+            />
+            <Product
+              title="Gundam Bael Metal Robot Tamashi 1/144"
+              price={999.99}
+              image="https://a.rimg.com.tw/s2/5/4e/64/22152614447716_386.jpg"
+              rating={3}
+            />
+            <Product
+              title="Gundam Bael Metal Robot Tamashi 1/144"
+              price={999.99}
+              image="https://a.rimg.com.tw/s2/5/4e/64/22152614447716_386.jpg"
+              rating={2}
+            />
+            
+         </div>
+
+         <div className="home_row">
+            <Product
+              title="Gundam Bael Metal Robot Tamashi 1/144"
+              price={999.99}
+              image="https://a.rimg.com.tw/s2/5/4e/64/22152614447716_386.jpg"
+              rating={4}
+            />
+         </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
