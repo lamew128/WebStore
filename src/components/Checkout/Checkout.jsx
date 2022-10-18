c76be47a423ba4c80b48742ce178d751837808ef
Eugene <lamew128@gmail.com>
2022-10-18 17:59:00 -0400

    add/remove items in checkout page

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from '../Subtotal/Subtotal';
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct';
+import { useStateValue } from '../../StateProvider';
 
 function Checkout() {
+
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className='checkout'>
 
       <div className="checkout_basket">
         <div>
           <h2 className="checkout_title">Basket</h2>
+          
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
